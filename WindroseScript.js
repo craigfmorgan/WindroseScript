69a09eb3cb5f90b1653bf868602f8ba991d94058
craigfmorgan <craig.morgan@impactiq.io>
2021-05-11 18:03:06 -0600

    test

--- WindroseScript.js
+++ WindroseScript.js
@@ -181,9 +181,9 @@ var metricSettings = {
 
 var layout = {
 	autosize: true,
-	//width: 700,
-	//height: 500,
-	/*margin: {
+	width: 700,
+	height: 500,
+	margin: {
 		l: 30,
 		r: 280,
 		b: 25,
@@ -191,7 +191,7 @@ var layout = {
 		pad: 2,
 		autoexpand: true
 	},
-	*/
+	
 	legend: {
 		tracegroupgap: 0,
 		font: {
